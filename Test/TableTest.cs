dd0013871fdac5ba37b5e75d95b9f622f323965e
maria <maria@DESKTOP-0HJT21N>
2020-02-27 11:44:24 +0100

    Column and table tests

--- Test/TableTest.cs
+++ Test/TableTest.cs
@@ -13,25 +13,57 @@ namespace Test
         [TestMethod]
         public void searchColumnName()
         {
+            BDcreation.BDcreatioon();
+            DataBase db = new DataBase();
+            db.load("BD");
 
         }
 
         [TestMethod]
         public void deleteColumn()
         {
+            BDcreation.BDcreatioon();
+            DataBase db = new DataBase();
+            db.load("BD");
 
+
+            db.deleteColumn("table", "column");
+            Table table = db.Tables["table"];
+            Assert.IsFalse(table.Columns.ContainsKey("column"));
         }
 
         [TestMethod]
         public void update()
         {
+            BDcreation.BDcreatioon();
+            DataBase db = new DataBase();
+            db.load("BD");
 
+            Table table = db.Tables["table"];
+            Column column = table.Columns["column"];
+            string data = column.list[1];
+
+            db.update("table", "column", "newdata", Operator.Equal, "data");
+
+            string newdata = column.list[1];
+
+            Assert.IsFalse(data == newdata);
         }
 
         [TestMethod]
         public void insert()
         {
+            BDcreation.BDcreatioon();
+            DataBase db = new DataBase();
+            db.load("BD");
 
+            db.insert("table", "column", "newData");
+
+
+            Table table = db.Tables["table"];
+            Column column = table.Columns["column"];
+
+            Assert.IsFalse(column.list.Contains("newData"));
         }
     }
 }
