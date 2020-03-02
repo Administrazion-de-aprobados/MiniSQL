361af1d54c556f0e9c440d6410a5bf6e153675c0
Ane Miren Echeverria <aneecheverria11@gmail.com>
2020-03-02 11:59:11 +0100

    corrections in write and load

--- Library/DataBase.cs
+++ Library/DataBase.cs
@@ -161,13 +161,15 @@ namespace Library
                         string columnName = line[0];
                         string dataType = line[1];
                         List<string> data = new List<string>();
+                        
+                        
                         for (int j = 2; j <= line.Length; j++)
                         {
 
                             data.Add(line[j]);
                         }
 
-                        tab.addToTable(columnName, data);
+                        tab.addToTable(columnName, dataType ,data);
 
 
                     }
